adba247e55470814839e94fb6bcab4aa755f0d26
karthik <karthik.venkata@kanini.com>
2021-01-27 12:03:29 +0530

    easy q1

---
+++ Easy q1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Employees = new List<string>();
+            Console.WriteLine("Please enter the employee names in the order of their eligibility for promotion(Please enter blank to stop)");
+            for (; ; )
+            {
+                String s = Console.ReadLine();
+                if (s == "") break;
+                else Employees.Add(s);
+            }
+
+            /*
+              
+            for (int i = 0; i < Employees.Count; i++)
+                Console.WriteLine(Employees[i]);
+            Console.ReadKey();
+
+             */
+        }
+    }
+}
